9aaff686ef53734ebe2ae1ab56fa658e87e52f0b
ciedecem <zhishanlee@163.com>
2014-01-02 23:44:05 +0800

    more performance version of BubbleSort

--- mrunit/src/main/java/org/apache/hadoop/examples/offgrid_sort/BubbleSort.java
+++ mrunit/src/main/java/org/apache/hadoop/examples/offgrid_sort/BubbleSort.java
@@ -24,6 +24,33 @@ public class BubbleSort {
 
     }
 
+    /*
+    * Note
+    * if there is no changes in a sort, it is not needed to sort continually. Because each element is already in order..
+    * */
+    public static <T extends Comparable<? super T>>
+    void bubbleSort_V2( T[] rawArr){
+        boolean needNextPass = true;
+
+        for(int outer= rawArr.length -1 ; outer>1 && needNextPass; outer--){       // outer loop (backward)
+            // Array may be sorted and next pass not needed.
+            needNextPass = false;
+
+            for(int inner=0; inner<outer; inner++){               // inner loop (forward)
+                if ( rawArr[inner].compareTo(rawArr[inner+1])>0){
+                    T tmp = rawArr[inner];
+                    rawArr[inner] = rawArr[inner +1];
+                    rawArr[inner+1] = tmp;
+
+                    needNextPass = true; // Next pass still need
+                }
+
+            }
+        }
+
+    }
+
+
     public static void main(String [] args){
         Integer [] iArray = {13,1, 24, 26, 2, 15, 38,27};
         BubbleSort.<Integer>bubbleSort(iArray);
---
+++ mrunit/src/test/java/org/apache/hadoop/examples/offgrid_sort/BubbleSortTest.java
@@ -0,0 +1,71 @@
+package org.apache.hadoop.examples.offgrid_sort;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhishan
+ * Date: 1/2/14
+ * Time: 9:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BubbleSortTest {
+
+    @Test(description = "Double type")
+    public void testBubbleSort() throws Exception {
+        Double [] doubles = {2.34, 1.0,0.344,2.54,0.46,3.2013, };
+        BubbleSort.<Double>bubbleSort(doubles);
+        Arrays.<Double>sort(doubles);
+        Double [] expects = {0.344,0.46, 1.0, 2.34, 2.54,3.2013};
+        Assert.assertEquals(doubles, expects, "Double type sort fails");
+    }
+    @Test(description = "String type")
+    public void testBubbleSort_String() throws Exception {
+        List<String> strList = new ArrayList<>();
+        strList.add("hello");
+        strList.add("world");
+        strList.add("cdfg");
+        strList.add("Cdfg");
+        strList.add("cDfg");
+        String [] actual = new String[strList.size()];
+        strList.<String>toArray(actual);
+        BubbleSort.<String>bubbleSort(actual);
+        //String[])strList.toArray(),
+        // java.lang.ClassCastException: [Ljava.lang.Object; cannot be cast to [Ljava.lang.String;
+
+        String [] expects = {"Cdfg", "cDfg", "cdfg","hello", "world"};
+        Assert.assertEquals( actual, expects, "String type sort fails");
+    }
+
+
+    @Test(description = "Double type")
+    public void testBubbleSort_V2() throws Exception {
+        Double [] doubles = {2.34, 1.0,0.344,2.54,0.46,3.2013, };
+        BubbleSort.<Double>bubbleSort(doubles);
+        Arrays.<Double>sort(doubles);
+        Double [] expects = {0.344,0.46, 1.0, 2.34, 2.54,3.2013};
+        Assert.assertEquals(doubles, expects, "Double type sort fails");
+    }
+    @Test(description = "String type")
+    public void testBubbleSort_String_V2() throws Exception {
+        List<String> strList = new ArrayList<>();
+        strList.add("hello");
+        strList.add("world");
+        strList.add("cdfg");
+        strList.add("Cdfg");
+        strList.add("cDfg");
+        String [] actual = new String[strList.size()];
+        strList.<String>toArray(actual);
+        BubbleSort.<String>bubbleSort(actual);
+        //String[])strList.toArray(),
+        // java.lang.ClassCastException: [Ljava.lang.Object; cannot be cast to [Ljava.lang.String;
+
+        String [] expects = {"Cdfg", "cDfg", "cdfg","hello", "world"};
+        Assert.assertEquals( actual, expects, "String type sort fails");
+    }
+}
